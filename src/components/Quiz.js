a3a3e96e1fb18f16c8c73a0e04ceee33ee0b82c0
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-20 13:00:54 +0700

    feat : answer check

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import QuestionsData from "../data/QuestionsData";
 
 const Quiz = () => {
   //ใช้เก็บลำดับคำถาม
   const [current , setCurrent] = useState(0) //เริ่มต้นที่ 0 คือ ให้เริ่่มคำถามจาก array index0 ใน data
   const [selectChoice , setSelectChoice] = useState('')
+
+  useEffect(() => {
+    checkAnswer()
+  },[selectChoice])
+
+  const checkAnswer = () => {
+    if(selectChoice != ""){
+if(selectChoice === QuestionsData[current].answer ){
+alert("Woww! you right ^0^");
+}else{
+  alert("Oops! you wrong T_T")
+}
+    }
+  }
+
   return (
     <div className="quiz">
       <h2>{QuestionsData[current].question}</h2>
