4f567f7e25a3d1f2f8b7e9c64272bd8a72146508
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-20 13:05:25 +0700

    feat:set score

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,36 +1,46 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import QuestionsData from "../data/QuestionsData";
+import { DataContext } from "../App";
 
 const Quiz = () => {
   //ใช้เก็บลำดับคำถาม
-  const [current , setCurrent] = useState(0) //เริ่มต้นที่ 0 คือ ให้เริ่่มคำถามจาก array index0 ใน data
-  const [selectChoice , setSelectChoice] = useState('')
+  const [current, setCurrent] = useState(0); //เริ่มต้นที่ 0 คือ ให้เริ่่มคำถามจาก array index0 ใน data
+  const [selectChoice, setSelectChoice] = useState("");
+  const { score, setScore } = useContext(DataContext);
 
   useEffect(() => {
-    checkAnswer()
-  },[selectChoice])
+    checkAnswer();
+  }, [selectChoice]);
 
   const checkAnswer = () => {
-    if(selectChoice != ""){
-if(selectChoice === QuestionsData[current].answer ){
-alert("Woww! you right ^0^");
-}else{
-  alert("Oops! you wrong T_T")
-}
+    if (selectChoice != "") {
+      if (selectChoice === QuestionsData[current].answer) {
+        alert("Woww! you right ^0^");
+        setScore(score+1)
+      } else {
+        alert("Oops! you wrong T_T");
+      }
     }
-  }
+  };
 
   return (
     <div className="quiz">
       <h2>{QuestionsData[current].question}</h2>
       <div className="choice">
-        <button onClick={()=>setSelectChoice("A")}>{QuestionsData[current].A}</button>
-        <button onClick={()=>setSelectChoice("B")}>{QuestionsData[current].B}</button>
-        <button onClick={()=>setSelectChoice("C")}>{QuestionsData[current].C}</button>
-        <button onClick={()=>setSelectChoice("D")}>{QuestionsData[current].D}</button>
-
+        <button onClick={() => setSelectChoice("A")}>
+          {QuestionsData[current].A}
+        </button>
+        <button onClick={() => setSelectChoice("B")}>
+          {QuestionsData[current].B}
+        </button>
+        <button onClick={() => setSelectChoice("C")}>
+          {QuestionsData[current].C}
+        </button>
+        <button onClick={() => setSelectChoice("D")}>
+          {QuestionsData[current].D}
+        </button>
       </div>
-      <p>{`${current+1} / ${QuestionsData.length}`}</p>
+      <p>{`${current + 1} / ${QuestionsData.length}`}</p>
     </div>
   );
 };
