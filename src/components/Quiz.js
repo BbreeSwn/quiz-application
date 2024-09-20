5534285947a2917b5b9b7e9a18670d24af18754d
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-20 12:42:05 +0700

    feat: get question data

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useState } from "react";
+import QuestionsData from "../data/QuestionsData";
 
 const Quiz = () => {
+  //ใช้เก็บลำดับคำถาม
+  const [current , setCurrent] = useState(0) //เริ่มต้นที่ 0 คือ ให้เริ่่มคำถามจาก array index0 ใน data
   return (
     <div className="quiz">
-<h1>Quiz</h1>
-    </div>
-  )
-}
+      <h2>{QuestionsData[current].question}</h2>
+      <div className="choice">
+        <button>{QuestionsData[current].A}</button>
+        <button>{QuestionsData[current].B}</button>
+        <button>{QuestionsData[current].C}</button>
+        <button>{QuestionsData[current].D}</button>
 
-export default Quiz
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
